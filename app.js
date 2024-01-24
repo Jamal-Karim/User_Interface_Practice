5ba7f8b474328b5246ed2fb8349759dd52d832e1
Jamal Karim <1jamalkarim@gmail.com>
2024-01-24 08:35:17 -0800

    add ui of mobile screen

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-
+//Dropdown Menu
 const dropDown1 = document.querySelector(".text1");
 const dropDown2 = document.querySelector(".text2");
 
@@ -13,7 +13,7 @@ let menu2Flag = false;
 
 dropDown1.addEventListener('click', () => {
     console.log('menu clicked');
-    if(!menu1Flag){
+    if (!menu1Flag) {
         dropDownOptions1.classList.add('show');
         menu1Flag = true;
     } else {
@@ -24,7 +24,7 @@ dropDown1.addEventListener('click', () => {
 
 dropDown2.addEventListener('click', () => {
     console.log('menu clicked');
-    if(!menu2Flag){
+    if (!menu2Flag) {
         dropDownOptions2.classList.add('show');
         menu2Flag = true;
     } else {
@@ -40,8 +40,8 @@ dropDownOptions1.querySelectorAll('li').forEach(element => {
         dropDown1Text.textContent = event.target.textContent;
         dropDownOptions1.classList.remove('show');
         menu1Flag = false;
-    })
-})
+    });
+});
 
 dropDownOptions2.querySelectorAll('li').forEach(element => {
     element.addEventListener('click', event => {
@@ -50,5 +50,7 @@ dropDownOptions2.querySelectorAll('li').forEach(element => {
         dropDown2Text.textContent = event.target.textContent;
         dropDownOptions2.classList.remove('show');
         menu2Flag = false;
-    })
-})
+    });
+});
+
+//Mobile Menu
